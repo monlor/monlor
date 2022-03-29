84a52c4828996331f4c6b687b9238c74d6f08ae9
monlor <monlor@qq.com>
2022-03-29 16:53:04 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,23 @@
+
+<h3 align="center">👋 Hello! I'm monlor.</h3>
+
+<p align="center">
+  <a href="https://www.monlor.com">Blog</a> •
+  <a href="https://github.com/monlor">GitHub</a>
+</p>
+
+### Github Statistics
+
+![Stats](https://github-readme-stats.vercel.app/api?username=monlor&show_icons=true&layout=compact&count_private=true&hide_title=true&theme=default&)
+![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=monlor&layout=compact&count_private=true&theme=default&hide=css,html,javascript)
+
+### Latest blog posts
+
+- 2022-01-30 [小米路由器Monlor-Tools工具箱2.0，支持小米路由最新固件](https://www.monlor.com/archives/9/)
+- 2022-01-21 [Keepalived 实现双路由负载均衡](https://www.monlor.com/archives/23/)
+- 2021-04-13 [Intel 11 代 CPU 成功吃上黑苹果](https://www.monlor.com/archives/20/)
+- 2021-03-24 [FRP内网穿透服务免费使用的详细教程](https://www.monlor.com/archives/33/)
+- 2021-03-24 [CentOS 7下Oracle 12c数据库安装教程](https://www.monlor.com/archives/26/)
+- 2021-03-24 [群晖上使用 Docker 搭建 Aria2 和迅雷远程服务，并配合 FRP 实现 Aria2 的远程下载](https://www.monlor.com/archives/25/)
+
+[>>> More blog posts](https://www.monlor.com/archive.html)
---
+++ update.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*
+import requests
+import xml.etree.ElementTree as ET
+import time
+import datetime
+
+with open('README.md', 'w') as f:
+    f.write(r'''
+<h3 align="center">👋 Hello! I'm monlor.</h3>
+
+<p align="center">
+  <a href="https://www.monlor.com">Blog</a> •
+  <a href="https://github.com/monlor">GitHub</a>
+</p>
+
+### Github Statistics
+
+![Stats](https://github-readme-stats.vercel.app/api?username=monlor&show_icons=true&layout=compact&count_private=true&hide_title=true&theme=default&)
+![Lang](https://github-readme-stats.vercel.app/api/top-langs/?username=monlor&layout=compact&count_private=true&theme=default&hide=css,html,javascript)
+
+### Latest blog posts
+
+''')
+    ret = requests.get('https://www.monlor.com/feed/')
+    ret.encoding='utf-8'
+    feed = ret.text
+    root = ET.fromstring(feed)
+    for item in root.findall('.//item'):
+        text = item.find("title").text
+        url = item.find("link").text
+        published = item.find("pubDate").text
+        time_format = datetime.datetime.strptime(published, '%a, %d %b %Y %H:%M:%S %z')
+        f.write('- {} [{}]({})\n'.format(time_format.strftime("%Y-%m-%d"), text, url))
+
+    f.write('''
+[>>> More blog posts](https://www.monlor.com/archive.html)
+''')
